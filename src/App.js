b7a61af3ad8f2acb79db7348a23ba4881e4a190b
Michael Fourie <59344613+michael-fourie@users.noreply.github.com>
2023-12-21 11:58:11 -0800

    funny monkey gif

--- src/App.js
+++ src/App.js
@@ -1,126 +1,184 @@
 import React, { useState, useEffect } from 'react';
 import './App.css'; // Import a CSS file for styling if needed
-import left from "./assets/output_image.jpeg"
+import left from "./assets/output_image.png";
+import bgGif from './assets/monkey-cute.gif';
 import telegramIcon from "./assets/telegram.png";
 import twitterIcon from "./assets/twitter.png";
-import {isMobile} from 'react-device-detect';
+import song from './assets/song.mp3';
 
 const App = () => {
-  const [showChat, setShowChat] = useState(false);
-  const [randomLine, setRandomLine] = useState('');
+  const divStyle = {
+    position: 'relative',
+    backgroundColor: 'white',
+    height: '100vh',
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    fontFamily: 'Comic Sans MS',
+    zIndex: 0,
+  };
 
-  const lines=[
-    "Left curve market particpants are highly unlikely outperform the S&P500. Infact they can barely maintain a longterm profitable PnL",
-    "Proud lobotomy survivor and activist",
-    "$dogswifhat would be a good additional to my 401k",
-    "My parents think I'm a really good trader",
-    "I don't know a single person irl who owns crypto",
-    "veH0rny is likely a fed",
-    "I bought the pico top of $BONK gonna wait to rotate out next cycle",
-    "I used to have Richard Hearts notifications on",
-    "I clicked a phishing link on my grandma's laptop and she got her Mekaverse NFT drained",
-    "This is the use-case"
-    
-  ]
+  const gifBackgroundStyle = {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    width: '100%',
+    height: '100%',
+    opacity: 0.45,
+    zIndex: -1,
+    backgroundImage: `url(${bgGif})`,
+    backgroundSize: 'cover',
+    backgroundPosition: 'center',
+    objectFit: 'cover', // Added object-fit property
+  };
+
+  const initialText = 'cLiCk mE';
+  const otherText = 'funny monkey gif'
+  const otherText2 = 'funny funny monkey gif'
+  const title = "$funnymonkeygif"
+  
+  const playAudio = () => {
+    const audio = new Audio(song);
+    audio.play();
+  };
+
+  const getRandomColor = () => {
+    return `rgb(${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)})`;
+  };
+
+  const [letters, setLetters] = useState(initialText.split('').map(() => getRandomColor()));
+  const [letters2, setLetters2] = useState(otherText.split('').map(() => getRandomColor()));
+  const [letters3, setLetters3] = useState(otherText2.split('').map(() => getRandomColor()));
+  const [letters4, setLetters4] = useState(title.split('').map(() => getRandomColor()));
+
+  const handleClick = () => {
+    setLetters(initialText.split('').map(() => getRandomColor()));
+    playAudio();
+  };
+
+  const textStyle = {
+    fontWeight: 'bold',
+    cursor: 'pointer',
+    fontSize: '72px',
+    transition: 'color 0.3s ease-in-out',
+  };
+
+  const textStyle2 = {
+    cursor: 'pointer',
+    fontSize: '32px',
+    transition: 'color 0.3s ease-in-out',
+  };
+
+  const rainbowBackground = {
+    background: 'linear-gradient(45deg, red, orange, yellow, green, blue, indigo, violet)',
+    fontSize: '22px',
+    color: 'black',
+    fontWeight: 'bold',
+    padding: '10px 20px',
+    borderRadius: '20px',
+    border: 'none',
+    margin: '10px',
+    cursor: 'pointer',
+    fontFamily: 'Comic Sans MS',
+    backgroundSize: '300% 300%',
+    transition: 'background 0.5s ease-in-out',
+  };
 
   useEffect(() => {
-    getRandomLine();
-  }, []); // Fetch a random line when the component mounts
+    const img = document.getElementById('movingImage');
+  
+    const moveImage = () => {
+      const x = Math.floor(Math.random() * 61) - 30; // Random value between -30 and 30 for X-axis movement
+      const rotation = Math.floor(Math.random() * 61) - 30; // Random value between -30 and 30 for rotation
 
-  const getRandomLine = async () => {
-    try {
-      const randomIndex = Math.floor(Math.random() * lines.length);
-      setRandomLine(lines[randomIndex]);
-    } catch (error) {
-      console.error('Error fetching data:', error);
-    }
-  };
-  const [buttonDisabled, setButtonDisabled] = useState(false);
+      img.style.transform = `translateX(${x}px) rotate(${rotation}deg)`;
+  
+      setTimeout(moveImage, 700); // Repeat the movement after 1 second (1000ms)
+    };
+  
+    moveImage(); // Start the movement initially
+  }, []);
+  
 
-  const disableButton = (e) => {
-    e.preventDefault();
-    e.target.innerText = "Coming soon";
-    setButtonDisabled(true);
-  };
-
-  const handleImageClick = () => {
-    setShowChat(!showChat);
-    if (!showChat) {
-      getRandomLine();
-    }
-  };
-  const redirectToURL = () => {
-    window.location.href = 'https://raydium.io/swap/?inputCurrency=sol&outputCurrency=73pRiEwXtrGdntsGbYagysrKgH6NwWjQBLyaoJZSdVH9';
-  };
-  const redirectToURL2 = () => {
-    window.location.href = 'https://dexscreener.com/solana/fu3sknnadh3y3xnn6abryx1lhtvr43sj2swtzq9gumk';
+  const redirectToURL = (url) => {
+    window.open(url, "_blank");
   };
 
-  let pSize = isMobile ? '14px' : '22px'
-  let picSize = isMobile ? "75%" : "45%"
-  let chatStyle = isMobile ? { width: "45%", position: 'absolute', right: '10%', top: '80%', transform: 'translateY(-50%)', backgroundColor: 'lightgray', padding: '10px', borderRadius: '10px', fontFamily: 'Comic Sans MS', fontSize: '16px' } : { width: "20%", position: 'absolute', right: '10%', top: '50%', transform: 'translateY(-50%)', backgroundColor: 'lightgray', padding: '10px', borderRadius: '10px', fontFamily: 'Comic Sans MS', fontSize: '26px' }
+  let pSize = '14px' ;
+  let picSize = "25%" ;
+
   return (
-    <div style={{ backgroundColor: 'white', height: '100vh', display: 'flex', justifyContent: 'center', alignItems: 'center', fontFamily: 'Comic Sans MS' }}>
+    <div style={divStyle}>
+      <div style={gifBackgroundStyle}></div>
       <div style={{ fontWeight: "bold", position: 'absolute', top: '20px', left: '20px', fontSize: '24px', color: 'black' }}>
-        $RETARD 
+        {letters4.map((color, index) => (
+            <span key={index} style={{ ...textStyle, color }}>{title[index]}</span>
+          ))}
       </div>
 
-      <div style={{ display: 'flex', gap: '2em', position: 'absolute', top: '20px', right: '50px' }}>
-        <a href="https://t.me/+OsESYEwWMbw3YjNh" target="_blank" rel="noopener noreferrer">
-          <img src={telegramIcon} alt="Telegram" style={{ width: '30px', height: '30px', marginRight: '10px', cursor: 'pointer' }} />
-        </a>
-        <a href="https://twitter.com/cyberetard" target="_blank" rel="noopener noreferrer">
-          <img src={twitterIcon} alt="Twitter" style={{ width: '30px', height: '30px', cursor: 'pointer' }} />
-        </a>
+      <div style={{ display: 'flex', gap: '2em', position: 'absolute', top: '20px', right: '50px', zIndex: 10 }}>
+        <img src={telegramIcon} alt="Telegram" style={{ width: '30px', height: '30px', cursor: 'pointer' }} onClick={() => redirectToURL("https://t.me/+OsESYEwWMbw3YjNh")} />
+        <img src={twitterIcon} alt="Twitter" style={{ width: '30px', height: '30px', cursor: 'pointer' }} onClick={() => redirectToURL("https://twitter.com/cyberetard")} />
       </div>
 
-      <div style={{ textAlign: 'center' }}>
-        <p style={{ fontSize: pSize, width: '50%', margin: 'auto' }}>a shitcoin with a cool edgy ticker. its not good investment. bcus its fuckin $RETARDed. built for left curve market participants. this is not a good coin. its a great coin. that is why it will pump. it was always meant to, since before the inception of Solana itself. embrace retardation. nothing else matters. or makes sense.</p>
+      
+      <div style={{ textAlign: 'center', position: 'relative', zIndex: 10 }}>
+      
+        <div onClick={handleClick}>
+          {letters.map((color, index) => (
+            <span key={index} style={{ ...textStyle, color }}>{initialText[index]}</span>
+          ))}
+        </div>
+
+        <p style={{ fontSize: pSize, width: '50%', margin: 'auto' }}>
+          
+        {letters2.map((color, index) => (
+            <span key={index} style={{ ...textStyle2, color }}>{otherText[index]}</span>
+          ))}
+
+        </p>
+        <p style={{ fontSize: pSize, width: '50%', margin: 'auto' }}>
+          
+        {letters3.map((color, index) => (
+            <span key={index} style={{ ...textStyle2, color }}>{otherText2[index]}</span>
+          ))}
+
+        </p>
+
         <button
-        style={{ fontSize: "22px", backgroundColor: 'lightblue', color: 'white', padding: '10px 20px', borderRadius: '20px', border: 'none', margin: '10px', cursor: 'pointer', fontFamily: 'Comic Sans MS' }}
-        onMouseOver={(e) => e.target.style.backgroundColor = 'skyblue'}
-        onMouseOut={(e) => e.target.style.backgroundColor = 'lightblue'}
-        onClick={() => redirectToURL()}
-        disabled={buttonDisabled}
-      >
-        {buttonDisabled ? "Coming soon" : "Buy $RETARD"}
-      </button>
+          style={rainbowBackground}
+          onMouseOver={(e) => e.target.style.backgroundColor = 'skyblue'}
+          onMouseOut={(e) => e.target.style.backgroundColor = 'lightblue'}
+          onClick={() => redirectToURL("https://raydium.io/swap/")}
+        >
+          Buy $funnymonkeygif
+        </button>
 
-      <button
-        style={{ fontSize: "22px", backgroundColor: 'lightblue', color: 'white', padding: '10px 20px', borderRadius: '20px', border: 'none', margin: '10px', cursor: 'pointer', fontFamily: 'Comic Sans MS' }}
-        onMouseOver={(e) => e.target.style.backgroundColor = 'skyblue'}
-        onMouseOut={(e) => e.target.style.backgroundColor = 'lightblue'}
-        onClick={() => redirectToURL2()}
-        disabled={buttonDisabled}
-      >
-        {buttonDisabled ? "Coming soon" : "Dextools"}
-      </button>
-
-        <img
-          src={left}
-          alt="Large Image"
-          style={{
-            width: picSize, 
-            height: 'auto',
-            display: 'block',
-            margin: 'auto',
-            cursor: 'pointer',
-            transition: 'transform 0.3s, width 0.3s',
-            fontFamily: 'Comic Sans MS'
-          }}
-          onClick={handleImageClick}
-          onMouseOver={(e) => e.target.style.transform = 'scale(0.97) rotate(3deg)'}
-          onMouseOut={(e) => e.target.style.transform = 'scale(1) rotate(0)'}
-        />
-                <p style={{ width: '50%', margin: 'auto' }}>click me for alpha</p>
-                <p style={{ width: '50%', margin: 'auto' }}>(over and over again)</p>
-
-        {showChat && (
-          <div style={chatStyle}>
-            {randomLine}
-          </div>
-        )}
+        <button
+          style={rainbowBackground}
+          onMouseOver={(e) => e.target.style.backgroundColor = 'skyblue'}
+          onMouseOut={(e) => e.target.style.backgroundColor = 'lightblue'}
+          onClick={() => redirectToURL("https://dexscreener.com/solana/")}
+        >
+          Dextools
+        </button>
       </div>
+
+      <img
+        id="movingImage"
+        src={left}
+        alt="Large Image"
+        style={{
+          width: picSize,
+          height: 'auto',
+          display: 'block',
+          margin: 'auto',
+          cursor: 'pointer',
+          transition: 'transform 0.3s, width 0.3s',
+          fontFamily: 'Comic Sans MS',
+          position: 'absolute',
+        }}
+      />
     </div>
   );
 };
